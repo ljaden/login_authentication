9afcc68a045cc73f2027b8ba5c761e00cb2f04d0
jaden <ljaden.9@gmail.com>
2022-04-19 14:55:07 -0400

    passportjs

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const express = require('express')
 const ejs = require('ejs')
 // require database - mongoose
 const mongoose = require('mongoose')
-// require encryption - mongoose
-const encrypt = require('mongoose-encryption')
+
+//==============================================================
+// require 
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+//==============================================================
+
 
 const app = express()
 // send static files to express
@@ -15,6 +21,18 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 // express middleware parser
 app.use(express.urlencoded({extended:true}))
+
+//==============================================================
+app.use(session({
+  secret: "Secret Session Key",
+  resave:false,
+  saveUninitialized:true
+}))
+
+// initialize passport
+app.use(passport.initialize())
+app.use(passport.session())
+//==============================================================
 
 // conenct to DB
 mongoose.connect('mongodb://127.0.0.1:27017/UserDB')
@@ -25,16 +43,46 @@ const loginSchema = mongoose.Schema({
   password:String
 })
 
-// secret password
-const secret = "secretpassword";
-loginSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password']})
+
+
+
+//==============================================================
+loginSchema.plugin(passportLocalMongoose)
+//==============================================================
+
+
+
 
 // mongoose model
 const User = mongoose.model('User',loginSchema)
 
+
+
+
+//==============================================================
+// create strategy with mongoose model User
+passport.use(User.createStrategy())
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//==============================================================
+
+
+
+
 // homepage
 app.get('/',(req,res) => {
   res.render('home')
+})
+
+// secrets
+app.get('/secrets', (req,res)=>{
+  if (req.isAuthenticated()){
+    res.render("secrets")
+  } else {
+    res.redirect('/login')
+  }
 })
 
 // login
@@ -48,33 +96,50 @@ app.get('/register', (req,res) => {
 })
 // register - save users data to database
 app.post('/register', (req,res) => {
-  User.create({email:req.body.username,password:req.body.password},(e,r)=>{
-    if(!e){
-      console.log(e);
-    }else { //render secrets page after user has succesfully registered
-      res.render('secrets')
+  const username = req.body.username
+  
+  
+  const UserSchema = new mongoose.Schema({
+    active: Boolean
+  })
+  // 
+  User.register({username:username},req.body.password, (error,user)=>{
+    if(error){
+      console.log(error);
+      res.redirect('/register')
+    } else {
+      passport.authenticate('local')(req,res,function(){
+        res.redirect("/secrets")
+      })
     }
   })
+
 })
 // login - authenticates users account to show secret page
 app.post('/login',(req,res) => {
   const loginEmail = req.body.username
   const loginPass = req.body.password
 
-  // console.log(loginEmail,loginPass)
-  
-  User.findOne({email:loginEmail},(e,r)=>{
-    if(e){ // if error, render back to login page
-      console.log(e);
-      res.render('login')
-    }else{ // no error
-      if(loginPass === r.password){ // check if passwords match
-        res.render('secrets')
-      }else { // doesnt match - render login page
-        res.render('login')
-      }
+  const user = new User({
+    username: loginEmail,
+    password: loginPass
+  })
+
+  req.logIn(user, (err)=>{
+    if(err){
+      console.log(err);
+    } else {
+      passport.authenticate('local')(req,res, () =>{
+        res.redirect('/secrets')
+      })
     }
   })
+})
+
+// LOGOUT
+app.get('/logout', (req,res) => {
+  req.logout()
+  res.redirect('/')
 })
 
 // port - listen
